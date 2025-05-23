4432e7a6d22ef8ebc051d3b32ce71f6f3674ee5c
nourjob <noursayfjob@gmail.com>
2025-05-23 14:48:09 -0700

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,15 @@
+// src/app/admin/layout.tsx
+
 import AdminDashboardLayout from "./components/layout";
 import Sidebar from "./dashboard/components/Sidebar";
 import Navbar from "./dashboard/components/Navbar";
 import Footer from "./dashboard/components/Footer";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <AdminDashboardLayout
       sidebar={<Sidebar />}
